9d4ee3c60aad710a4e456e04b3d12e0fb12b581f
JamesCurtis <jamescurtis@foxmail.com>
2023-01-28 13:02:12 +0800

    chore: update

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function list() {
   return slice(arguments);
 }
 var list1 = list(1, 2, 3); // [1, 2, 3]
+
+
+const array = [1, 1, 2, 3, 5, 5, 1]
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray); // [1, 2, 3, 5]
+
