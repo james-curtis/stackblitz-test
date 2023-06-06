8e7a1035c4dd0f103b78dae275bcb9e284e6bbd2
JamesCurtis <jamescurtis@foxmail.com>
2023-06-06 13:02:12 +0800

    chore: update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 Math.random().toString(16).substring(2); //14位 Math.random().toString(36).substring(2); //11位
 
-
+var a = [1,2,3,7,8,9]; var b = [4,5,6]; var insertIndex = 3;
+a.splice.apply(a, Array.concat(insertIndex, 0, b)); // a: 1,2,3,4,5,6,7,8,9
