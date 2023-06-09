9944c2c6e0caccbf6317546fff8562b259c2c08d
JamesCurtis <jamescurtis@foxmail.com>
2023-06-09 13:02:12 +0800

    chore: update

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ Math.random().toString(16).substring(2); //14位 Math.random().toString(36).subs
 
 var a = [1,2,3,7,8,9]; var b = [4,5,6]; var insertIndex = 3;
 a.splice.apply(a, Array.concat(insertIndex, 0, b)); // a: 1,2,3,4,5,6,7,8,9
+
+var temp = new Date();
+var regex = /\//g;
+(temp.toLocaleDateString() + ' ' + temp.toLocaleTimeString().slice(2)).replace(regex,'-');	// "2015-5-7 9:04:10"
