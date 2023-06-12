44010d9ce6b6ed7a4a9311897d1c328709240778
JamesCurtis <jamescurtis@foxmail.com>
2023-06-12 13:02:12 +0800

    chore: update

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ a.splice.apply(a, Array.concat(insertIndex, 0, b)); // a: 1,2,3,4,5,6,7,8,9
 var temp = new Date();
 var regex = /\//g;
 (temp.toLocaleDateString() + ' ' + temp.toLocaleTimeString().slice(2)).replace(regex,'-');	// "2015-5-7 9:04:10"
+
+(new Date).valueOf();  	// 1431004132641
